ad21fa7d4e35221e40b6b542ec6c38e3784b11fd
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-22 10:32:30 +0900

    拡張子の変更とApp.jsのエラーの解消

--- src/App.js
+++ src/App.js
@@ -70,6 +70,7 @@ function App() {
 
   useEffect(() => {
     getCountryData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [country])
 
   useEffect(() => {
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,4 +1,4 @@
-const Title = ({title}) => {
+const Title = ({ title }: { title:string }) => {
   return <h1>{title}</h1>
 }
 
